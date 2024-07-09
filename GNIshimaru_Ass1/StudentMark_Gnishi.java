b4ec5b7eed9579b6e0659c09496e31cd6a9694a2
GUILHERME YOITI NISHIMARU <guilhermeyoitinishimaru@gmail.com>
2024-07-10 00:34:52 +0800

    comments

--- GNIshimaru_Ass1/StudentMark_Gnishi.java
+++ GNIshimaru_Ass1/StudentMark_Gnishi.java
@@ -15,26 +15,26 @@ public class StudentMark_Gnishi
     public static void main(String[] args) {
         
        int numberOfStudents = 30;
-       float[] marks = new float[numberOfStudents];
-       Scanner scanner = new Scanner(System.in); 
+       float[] marks = new float[numberOfStudents]; //it is necessary to create the array and the number of values inside it
+       Scanner scanner = new Scanner(System.in); // it is necessary to create an object to allocate memory to the class, in this case it will read the data being written
        
         System.out.println("Input assignment name");
         
-        String name = scanner.nextLine();
+        String name = scanner.nextLine(); //reads the name inputed and assign the value to "name" in string
         
         System.out.println("Input 30 student marks");
         
         for(int i=0; i< numberOfStudents; i++) {
             float mark = scanner.nextFloat(); //reads the data introduced
             
-            //marks[i] = scanner.nextFloat();
             if (mark < 0 || mark > 30){
                 System.out.println("Invalid marks, 0 < mark < 30, please enter again");
-                i--; //decrease 1 to go back to the same mark
+                i--; //decrease 1 to go back to the same mark and allow to enter again the mark
             } else {
                 marks[i] = mark; //store the marks that is correct
             }
         }
+        
         //starting the variables to compare
         float largest = marks[0];
         float smallest = marks [0];
@@ -47,11 +47,11 @@ public class StudentMark_Gnishi
                 smallest = marks[i];
             }
         }
+        
         System.out.println("Assignment name: " + name);
         System.out.println("Entered marks: ");
         System.out.println("Largest mark: " + largest);
         System.out.println("Smallest mark: " + smallest);
-
         for(int j=0; j<numberOfStudents; j++ ) {
             System.out.println(marks[j]);
         }}}
