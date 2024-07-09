077c156e2a1a5f1fd16c7a993c3470672edb5786
GUILHERME YOITI NISHIMARU <guilhermeyoitinishimaru@gmail.com>
2024-07-09 19:28:34 +0800

    First settings

---
+++ StudentMark_Gnishi.java
@@ -0,0 +1,26 @@
+import java.awt.Paint;
+
+/**
+ * Escreva uma descrição da classe StudentMark_Gnishi aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+
+import java.util.Scanner;
+
+public class StudentMark_Gnishi
+{
+    
+    public static void main(String[] args) {
+        
+       int numberOfStudents = 30;
+       float[] marks = new float[numberOfStudents];
+    
+        System.out.println("Input 30 student marks");
+        
+        for(int i=0; i< numberOfStudents; i++) {
+        
+        }
+    }
+}
