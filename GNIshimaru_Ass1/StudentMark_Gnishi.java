069797424e95fba72c45b41008933170b4e8c5ac
GUILHERME YOITI NISHIMARU <guilhermeyoitinishimaru@gmail.com>
2024-07-09 21:14:06 +0800

    Checking incorrect marks

--- GNIshimaru_Ass1/StudentMark_Gnishi.java
+++ GNIshimaru_Ass1/StudentMark_Gnishi.java
@@ -16,11 +16,23 @@ public class StudentMark_Gnishi
         
        int numberOfStudents = 30;
        float[] marks = new float[numberOfStudents];
+       Scanner scanner = new Scanner(System.in);
     
         System.out.println("Input 30 student marks");
         
         for(int i=0; i< numberOfStudents; i++) {
-        
+            float mark = scanner.nextFloat();
+            
+            //marks[i] = scanner.nextFloat();
+            if (mark < 0 || mark > 30){
+                System.out.println("Invalid marks, please enter again");
+                i--;
+            } else {
+                marks[i] = mark; //store the marks that is correct
+            }
         }
-    }
-}
+        System.out.println("Entered marks: ");
+        for(int j=0; j<numberOfStudents; j++ ) {
+            System.out.println(marks[j]);
+        }}}
+
