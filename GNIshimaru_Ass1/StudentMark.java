b1791fdf60741870f48abdf9b3ae00fb421e0bf8
GUILHERME YOITI NISHIMARU <guilhermeyoitinishimaru@gmail.com>
2024-07-15 13:28:37 +0800

    data validation

--- GNIshimaru_Ass1/StudentMark.java
+++ GNIshimaru_Ass1/StudentMark.java
@@ -9,6 +9,7 @@ import java.awt.Paint;
  */
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class StudentMark
 {
@@ -20,12 +21,23 @@ public class StudentMark
        Scanner scanner = new Scanner(System.in); // it is necessary to create an object to allocate memory to the class, in this case it will read the data being written
        
         System.out.println("Input assignment name");
+        //String name = scanner.nextLine(); //reads the name inputed and assign the value to "name" in string
         
-        String name = scanner.nextLine(); //reads the name inputed and assign the value to "name" in string
-        
+        //block and warn about wrong inpputs in name
+        String name = ""; //assign the valune name in string
+        while (true) {
+            name = scanner.nextLine();
+            if (!name.matches("[a-zA-Z ]+")) { //check if the input has only uppercase and lowercase letters
+            System.out.println("Invalid name, please enter a name containing only letters");
+            } else {
+                System.out.println("Valid name: "+ name); //address the valid name
+                break;
+            }
+        }
         System.out.println("Input 30 student marks");
         
         for(int i=0; i< numberOfStudents; i++) {
+            try{ //try and catch to detect problems according to the specification needed
             float mark = scanner.nextFloat(); //reads the data introduced
             
             if (mark < 0 || mark > 30){
@@ -34,6 +46,12 @@ public class StudentMark
             } else {
                 marks[i] = mark; //store the marks that is correct
             }
+        } catch (InputMismatchException e) { //catch shows the specification, in this case, not being an numeric value
+            //InputMismatchException returns an error when it detects an input of a different type than expected
+            System.out.println("Invalid input, please enter a numeric input"); 
+            scanner.next(); //get over with the invalid input
+            i--;
+        }
         }
         
         //creating the variables to compare
@@ -85,7 +103,7 @@ public class StudentMark
         System.out.println(" Mean:" + mean);
         System.out.println(" Standard Deviation:" + standardDeviation);
         
-        //block and warn about wrong inpputs in name
+        
         //do encapsulation in the largest and smallest
         //block strings inputs in marks
         //data validation
