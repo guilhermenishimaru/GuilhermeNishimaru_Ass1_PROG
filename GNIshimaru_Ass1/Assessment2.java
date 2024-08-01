3e3d0d1a99c0feee3bcb3a975947a95faf95c19c
GUILHERME YOITI NISHIMARU <guilhermeyoitinishimaru@gmail.com>
2024-08-01 22:39:30 +0800

    adjust1

--- GNIshimaru_Ass1/Assessment2.java
+++ GNIshimaru_Ass1/Assessment2.java
@@ -5,11 +5,17 @@ import java.util.Scanner;//to be able to scan waht is inputed by the user
 public class Assessment2
 {
    public static void main(String[] args) {
+       Scanner consoleScanner = new Scanner(System.in);
+       
+       System.out.print("Enter the path to the file: ");
+       String filePath = consoleScanner.nextLine(); //read the path inputed
+       
    try {
         File file = new File("C:\\students_grade.txt");   //show the path to the file
         Scanner myScanner = new Scanner(file);  //create scanner object to read the file
         while (myScanner.hasNextLine()) {              //read the file line by line
             String line = myScanner.nextLine();
+            
             System.out.println(line);
         }
         myScanner.close();                       //close the scanner
