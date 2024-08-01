164cd7a8261bc74c9c43044505f222a0081bf56c
GUILHERME YOITI NISHIMARU <guilhermeyoitinishimaru@gmail.com>
2024-08-01 22:57:13 +0800

    filter information (delete comments)

--- GNIshimaru_Ass1/Assessment2.java
+++ GNIshimaru_Ass1/Assessment2.java
@@ -13,9 +13,18 @@ public class Assessment2
    try {
         File file = new File("C:\\students_grade.txt");   //show the path to the file
         Scanner myScanner = new Scanner(file);  //create scanner object to read the file
+        
+        //print header: unit name
+        if (myScanner.hasNextLine()) {
+            String header = myScanner .nextLine();
+            System.out.println(header);
+        }
+        
+        //Read and print the rest
         while (myScanner.hasNextLine()) {              //read the file line by line
             String line = myScanner.nextLine();
-            
+            String[] parts = line.split("//");         //separate the cooments after "//" 
+            StringBuilder cleanedLine = new StringBuilder(parts[0].trim());  //stringbuilder allows to modify the same object without creating new ones and trim removes any write spaces
             System.out.println(line);
         }
         myScanner.close();                       //close the scanner
