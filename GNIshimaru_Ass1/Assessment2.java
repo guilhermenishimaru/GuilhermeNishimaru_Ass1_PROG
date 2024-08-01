2f511b9e3da97985cc6c970a1706499e49c04838
GUILHERME YOITI NISHIMARU <guilhermeyoitinishimaru@gmail.com>
2024-08-01 22:03:37 +0800

    read file

--- GNIshimaru_Ass1/Assessment2.java
+++ GNIshimaru_Ass1/Assessment2.java
@@ -1,7 +1,22 @@
-
+import java.io.*; //import library package
+import java.io.FileNotFoundException;
+import java.util.Scanner;//to be able to scan waht is inputed by the user
 
 public class Assessment2
 {
    public static void main(String[] args) {
-    }
+   try {
+        File file = new File("C:\\students_grade.txt");   //show the path to the file
+        Scanner myScanner = new Scanner(file);  //create scanner object to read the file
+        while (myScanner.hasNextLine()) {              //read the file line by line
+            String line = myScanner.nextLine();
+            System.out.println(line);
+        }
+        myScanner.close();                       //close the scanner
+   }
+   catch (FileNotFoundException e) {
+       System.out.println("An error has occured: File not found.");
+       e.printStackTrace();
+   }
+}
 }
