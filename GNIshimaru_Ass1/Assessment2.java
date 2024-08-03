5c8643c7119900dc0fb4c82b47079dcfd8a509c2
GUILHERME YOITI NISHIMARU <guilhermeyoitinishimaru@gmail.com>
2024-08-03 21:41:56 +0800

    appending total marks and printing

--- GNIshimaru_Ass1/Assessment2.java
+++ GNIshimaru_Ass1/Assessment2.java
@@ -9,7 +9,9 @@ public class Assessment2
        
        System.out.print("Enter the path to the file: ");
        String filePath = consoleScanner.nextLine(); //read the path inputed
+       
        int NumberOfStudents = 0; //create the variable to count the number of students
+       
    try {
         File file = new File("C:\\students_grade.txt");   //show the path to the file
         Scanner myScanner = new Scanner(file);  //create scanner object to read the file
@@ -20,30 +22,39 @@ public class Assessment2
             System.out.println(header);
         }
         
-        //Read and print the rest
+        StringBuilder finalLines = new StringBuilder();
+        
+        //Reads, calculates and print the marks with the total marks included
         while (myScanner.hasNextLine()) {              //read the file line by line
             String line = myScanner.nextLine();
             NumberOfStudents++;  //count by adding one unit for each line scanned
             
-                        
+            //Remove all the comments
             String[] parts = line.split("//");         //separate the cooments after "//" 
             StringBuilder cleanedLine = new StringBuilder(parts[0].trim());  //stringbuilder allows to modify the same object without creating new ones and trim removes any write spaces
+            
+            //Calculating total marks
             String[] Parts = cleanedLine.toString().split(",");
             int totalMark = 0;
             for (int i = Parts.length - 3; i < Parts.length; i++) {
                 totalMark += Integer.parseInt(Parts[i].trim());
             }
-            //for (int i=0; i<NumberOfStudents; i++);
             
-            
-            //System.out.println(line);
+            //adding the total marks at the end of each line
+            cleanedLine.append("Total Mark: ").append(totalMark);
+            finalLines.append(cleanedLine.toString().trim()).append("\n");
         }
+        
+        System.out.println(finalLines.toString());
+    
         //Separate the students grades
         myScanner.close();                       //close the scanner
-   }
-   catch (FileNotFoundException e) {
+   } catch (FileNotFoundException e) {
        System.out.println("An error has occured: File not found.");
        e.printStackTrace();
    }
+   consoleScanner.close();
 }
 }
+
+
